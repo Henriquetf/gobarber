6c46aaed468770b073d052b82d5445a8f486fe84
Henrique Torres <henriquetfw@gmail.com>
2020-08-16 00:09:54 -0300

    chore(mobile): add custom Roboto Slab fonts

---
+++ mobile/src/styles/fonts/RobotoSlab.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components/native';
+
+export const RobotoSlabRegular = css`
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const RobotoSlabMedium = css`
+  font-family: 'RobotoSlab-Medium';
+`;
